0632fa74428602201407d4fb09667943fcb9e462
Celia <celiapdg@hotmail.com>
2022-06-17 11:33:23 +0200

    exercise 20 with transition

--- ejercicio20/main.js
+++ ejercicio20/main.js
@@ -2,18 +2,32 @@ const on = document.getElementById("on");
 const off = document.getElementById("off");
 const bon = document.getElementById("bon");
 const boff = document.getElementById("boff");
+const divon = document.getElementById("div-on");
+const divoff = document.getElementById("div-off");
 
 function encender() {
-    off.hidden = true;
-    on.hidden = false;
+    // ocultamos y llevamos al fondo
+    off.style.opacity = "0";
+    boff.style.opacity = "0";
+    divoff.style.zIndex = "-1";
+    // mostramos y llevamos al frente
+    on.style.opacity = "1";
+    bon.style.opacity = "1";
+    divon.style.zIndex = "1";
 
 
 
 }
 
 function apagar() {
-    on.hidden = true;
-    off.hidden = false;
+    // ocultamos y llevamos al fondo
+    on.style.opacity = "0";
+    bon.style.opacity = "0";
+    divon.style.zIndex = "-1";
+    // mostramos y llevamos al frente
+    off.style.opacity = "1";
+    boff.style.opacity = "1";
+    divoff.style.zIndex = "1";
 }
 
 boff.addEventListener("click", encender);
